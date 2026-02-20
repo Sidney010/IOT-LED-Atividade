6f05ff07faeaf3eb56645edc7264d6c5533c4ff0
SIDNEY CAMPOS ARAGÃO <25132876@CFP127ALUNO.LOCAL>
2026-02-20 16:53:12 -0300

    LED

---
+++ sketch.ino
@@ -0,0 +1,16 @@
+// C++ code
+
+const int led = 13;
+
+void setup()
+{
+  pinMode(led, OUTPUT);//FUNÇÃO DE SAÍDA DE PINO 13
+}
+
+void loop()
+{
+  digitalWrite(led, HIGH);//Acende o led
+  delay(1000);//Por 1 segundos
+  digitalWrite(led, LOW);//Apaga o led
+  delay(1000);//Por 1 segundos
+ }  
